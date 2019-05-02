fcc448e06781c6a2c9ddfe70c1f17fd1516b830c
kriskoder <kris.koder@o2.pl>
2019-05-02 15:56:55 +0200

    Wydzielenie warstwy serwisu w części User

--- src/main/java/pl/coderslab/app/user/User.java
+++ src/main/java/pl/coderslab/app/user/User.java
@@ -1,9 +1,10 @@
 package pl.coderslab.app.user;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Email;
 
 import javax.persistence.*;
-
+import javax.validation.constraints.Size;
 
 
 @Entity
@@ -17,7 +18,10 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Size(min = 2)
     private String name;
+    @Email
     private String email;
+    @Size(min = 2)
     private String password;
 }
--- src/main/java/pl/coderslab/app/user/UserController.java
+++ src/main/java/pl/coderslab/app/user/UserController.java
@@ -4,36 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import pl.coderslab.app.training.Training;
-import pl.coderslab.app.training.TrainingRepository;
-import pl.coderslab.app.history.HistoryRepository;
+import pl.coderslab.app.training.TrainingService;
 
 import javax.servlet.http.HttpSession;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
+
 
 @Controller
 @RequestMapping("user")
-@SessionAttributes({"userSession", "training"})
 public class UserController {
 
     @Autowired
-    UserService userService;
-
-    @Autowired
-    HistoryRepository historyRepository;
-
-    @Autowired
-    TrainingRepository trainingRepository;
+    TrainingService trainingService;
 
     @RequestMapping("")
     public String userHome(Model model, HttpSession session) {
         User user = (User)session.getAttribute("userSession");
-        List<Training> trainingList = trainingRepository.findFirst3ByUserIdOrderByCreatedDesc(user.getId());
-        model.addAttribute("lastTrainings", trainingList);
+        model.addAttribute("lastTrainings", trainingService.getLast3TrainingsByUserId(user.getId()));
         return "userHome";
     }
-
-
 }
