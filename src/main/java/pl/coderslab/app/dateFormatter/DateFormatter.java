8d7d629f3f7ca0275a1508e7a99a03ebce329807
kriskoder <kris.koder@o2.pl>
2019-05-02 13:47:13 +0200

    Ddanie klasy formatującej datę wyświetlaną na froncie

---
+++ src/main/java/pl/coderslab/app/dateFormatter/DateFormatter.java
@@ -0,0 +1,13 @@
+package pl.coderslab.app.dateFormatter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateFormatter {
+
+    public static String formatDate(LocalDateTime dateToFormat){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        return dateToFormat.format(formatter);
+    }
+}
